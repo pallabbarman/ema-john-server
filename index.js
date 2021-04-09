3f523f9bc74d30a85b9040edb522060b400610e0
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-04-09 14:58:36 +0600

    search added

--- index.js
+++ index.js
@@ -31,9 +31,12 @@ client.connect((err) => {
     });
 
     app.get("/products", (req, res) => {
-        productsCollection.find({}).toArray((err, documents) => {
-            res.send(documents);
-        });
+        const search = req.query.search;
+        productsCollection
+            .find({ name: { $regex: search } })
+            .toArray((err, documents) => {
+                res.send(documents);
+            });
     });
 
     app.get("/product/:key", (req, res) => {
